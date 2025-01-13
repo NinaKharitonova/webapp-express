87d916d49e58d4b0f65d5d4a57cf9c15db891d6c
NinaKharitonova <ninakharitonova95@email.com>
2025-01-13 22:46:06 +0100

    Aggiunta di cors

--- app.js
+++ app.js
@@ -3,16 +3,25 @@ require("dotenv").config();
 // # INIT EXPRESS
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const host = process.env.APP_HOST;
 const port = process.env.APP_PORT;
+const frontEnd = process.env.APP_FRONTEND_URL;
+
+// # CORS CONFIGURATION
+const corsOptions = {
+  origin: frontEnd,
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 // # REGISTERING MIDDLEWARES
 app.use(express.json());
 app.use(express.static("public"));
+app.use(cors(corsOptions));
 
 // # REGISTERING ROUTER
 const webRouter = require("./routers/webRouter");
-app.use("/web", webRouter);
+app.use("/api/movie", webRouter);
 
 // # ERROR HADLERS
 const notFound = require("./middlewares/notFound");
